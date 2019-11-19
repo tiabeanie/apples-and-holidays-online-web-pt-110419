c0ce85b0ceb06b54262ee6531c4e83aa1b974d64
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-19 23:57:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -5,8 +5,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter][:christmas] << "Balloons"
-  holiday_hash[:winter][:new_years] << "Balloons"
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
 end
 
 
